de7fec906175a2f2f14dfd73d190993dc761b575
pavanbarman560 <pavanbarman560@gmail.com>
2024-10-16 20:22:16 +0530

    production ready

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import path from 'path';
+import connectDB from './config/db.js';
+import router from './routes/index.js';  // Make sure to add `.js` when using ES Modules
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", router);
+
+const PORT = process.env.PORT || 8080;
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, "/frontend/build")));
+
+app.get('*', (_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"));
+});
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("connected to DB");
+        console.log("server is now running");
+    });
+});
